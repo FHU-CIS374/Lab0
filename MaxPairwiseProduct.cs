6ad0bf6352932bbb114b6c2ce485f481f364d37e
Kenan Casey <kenancasey@gmail.com>
2020-01-13 10:47:04 -0600

    initial commit

---
+++ MaxPairwiseProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Lab0
+{
+    public class MaxPairwiseProduct
+    {
+
+        public static int NaiveMaxPairwiseProduct(int[] integers)
+        {
+            return 0;
+        }
+
+
+        public static int BetterMaxPairwiseProduct(int[] integers)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mpp = 0;
+            int[] numbers = { 1, 2, 10, 90, 10, 2, 1};
+            mpp = MaxPairwiseProduct.NaiveMaxPairwiseProduct(numbers);
+            Console.WriteLine(mpp);
+
+            mpp = MaxPairwiseProduct.BetterMaxPairwiseProduct(numbers);
+            Console.WriteLine(mpp);
+        }
+    }
+}
